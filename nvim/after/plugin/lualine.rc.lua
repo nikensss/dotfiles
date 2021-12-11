c257f73a574184bf5de50e710e2198d8c23b4dd1
Ricard Mallafre <rikitzzz@gmail.com>
2021-12-11 18:08:02 +0100

    fix: lualine warning about lsp

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -18,7 +18,7 @@ lualine.setup({
 		lualine_x = {
 			{
 				"diagnostics",
-				sources = { "nvim_lsp" },
+				sources = { "nvim_diagnostic" },
 				symbols = { error = " ", warn = " ", info = " ", hint = " " },
 			},
 			"encoding",
