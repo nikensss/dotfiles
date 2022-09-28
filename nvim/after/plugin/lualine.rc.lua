0044aedd88f48f02ec26c34be7363502ca8f9e3f
Ricard Mallafre <rikitzzz@gmail.com>
2022-09-28 22:13:02 +0200

    fix: lualine is now properly configured

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -1,29 +1,16 @@
-local status, lualine = pcall(require, "lualine")
-if not status then
-	return
-end
-
-lualine.setup({
+require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "solarized_dark",
-		section_separators = { "", "" },
-		component_separators = { "", "" },
+		theme = "tokyonight",
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
 		disabled_filetypes = {},
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch" },
-		lualine_c = { { "filename", file_status = true, path = 1 } },
-		lualine_x = {
-			{
-				"diagnostics",
-				sources = { "nvim_diagnostic" },
-				symbols = { error = " ", warn = " ", info = " ", hint = " " },
-			},
-			"encoding",
-			"filetype",
-		},
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { "filename" },
+		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
