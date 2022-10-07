a83d7297f3cdc1b6005857026b2106a65e31cb3c
Ricard Mallafre <rikitzzz@gmail.com>
2022-10-07 12:57:13 +0200

    fix: added back full file path in lualine

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -9,7 +9,7 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", file_status = true, path = 1 } },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
