dc674c25f0d955d12bac490369b4e89748d17444
Ricard Mallafre <rikitzzz@gmail.com>
2022-10-28 17:51:37 +0200

    chore: debugger markers update

--- nvim/after/plugin/debugging.lua
+++ nvim/after/plugin/debugging.lua
@@ -21,7 +21,7 @@ dap.configurations.typescript = {
 
 require("dap").set_log_level("INFO")
 dap.defaults.fallback.terminal_win_cmd = "20split new"
-vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
+vim.fn.sign_define("DapStopped", { text = "", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapBreakpoint", { text = "⏹", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapBreakpointCondition", { text = "⏸", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapBreakpointRejected", { text = "❌", texthl = "", linehl = "", numhl = "" })
