dc2d65232ddbe3d8b77ffc24d35851cbc88078dd
Ricard Mallafre <rikitzzz@gmail.com>
2023-02-08 22:23:14 +0100

    fix: resolve <leader>hs conflict

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
 vim.keymap.set('n', '<leader>ha', mark.add_file)
-vim.keymap.set('n', '<leader>hs', ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>hf', ui.toggle_quick_menu)
 
 vim.keymap.set('n', '<leader>h1', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<leader>h2', function() ui.nav_file(2) end)
