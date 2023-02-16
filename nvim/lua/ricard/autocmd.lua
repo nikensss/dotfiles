246aebf7aa095954cdca6cd67beae02ea648f19f
Ricard Mallafre <rikitzzz@gmail.com>
2023-02-16 13:15:19 +0100

    fix: added strategy to format md on save
    I added it commented out because it doesn't really work: the file is
    saved, then formatted. So then I have to save it again. I don't like
    that. I might also not be able to find this solution again, so I add it
    as a comment just in case.

--- nvim/lua/ricard/autocmd.lua
+++ nvim/lua/ricard/autocmd.lua
@@ -5,3 +5,13 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.spell = true
   end,
 })
+
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   desc = 'format markdown on write using prettier',
+--   group = vim.api.nvim_create_augroup('md_prettier_on_save', { clear = true }),
+--   callback = function(opts)
+--     if vim.bo[opts.buf].filetype == 'markdown' then
+--       vim.cmd.Prettier()
+--     end
+--   end,
+-- })
