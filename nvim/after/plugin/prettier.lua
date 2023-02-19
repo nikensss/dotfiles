3526f3f821fd46eed2c0f807cab2218c9b0aba4a
Ricard Mallafre <rikitzzz@gmail.com>
2023-02-19 19:16:28 +0100

    fix: removed unnecessary setup

--- nvim/after/plugin/prettier.lua
+++ nvim/after/plugin/prettier.lua
@@ -6,18 +6,4 @@ local prettier = require('prettier')
 
 prettier.setup({
   bin = 'prettierd', -- or 'prettier'
-  filetypes = {
-    'css',
-    'graphql',
-    'html',
-    'javascript',
-    'javascriptreact',
-    'json',
-    'less',
-    'markdown',
-    'scss',
-    'typescript',
-    'typescriptreact',
-    'yaml',
-  },
 })
