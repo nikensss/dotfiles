b2dcde5424051e77099f3b5b5f3f70df91efee0a
Ricard Mallafre <rikitzzz@gmail.com>
2023-02-23 20:54:41 +0100

    feat(remaps): paste without yank and format file remaps

--- nvim/lua/ricard/remap.lua
+++ nvim/lua/ricard/remap.lua
@@ -3,6 +3,8 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>w', vim.cmd.write)
 vim.keymap.set('n', '<leader>qa', vim.cmd.quitall)
 
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
@@ -17,7 +19,7 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 
 vim.keymap.set('n', 'Q', '<nop>')
 
-vim.keymap.set('n', '<leader>p', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format)
 
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
