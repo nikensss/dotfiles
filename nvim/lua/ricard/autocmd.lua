29d6417959f724f89a385ef5ffb6930c8568eb19
Ricard Mallafre <rikitzzz@gmail.com>
2023-03-06 12:03:23 +0100

    feat: autoformat markdown files with prettier

--- nvim/lua/ricard/autocmd.lua
+++ nvim/lua/ricard/autocmd.lua
@@ -3,15 +3,16 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+    vim.opt_local.textwidth = 80
   end,
 })
 
--- vim.api.nvim_create_autocmd('BufWritePre', {
---   desc = 'format markdown on write using prettier',
---   group = vim.api.nvim_create_augroup('md_prettier_on_save', { clear = true }),
---   callback = function(opts)
---     if vim.bo[opts.buf].filetype == 'markdown' then
---       vim.cmd.Prettier()
---     end
---   end,
--- })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  desc = 'format markdown on write using prettier',
+  group = vim.api.nvim_create_augroup('md_prettier_on_save', { clear = true }),
+  callback = function(opts)
+    if vim.bo[opts.buf].filetype == 'markdown' then
+      vim.cmd.Prettier()
+    end
+  end,
+})
