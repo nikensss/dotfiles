307f413f3ae7ac2cf02dce39a21eac21955d5837
Ricard Mallafre <rikitzzz@gmail.com>
2023-03-06 16:08:58 +0100

    fix: have null-ls format with prettier

--- nvim/after/plugin/prettier.lua
+++ nvim/after/plugin/prettier.lua
@@ -2,13 +2,12 @@ local null_ls = require('null-ls')
 
 local group = vim.api.nvim_create_augroup('lsp_format_on_save', { clear = false })
 local event = 'BufWritePre' -- or 'BufWritePost'
-local async = event == 'BufWritePost'
 
 null_ls.setup({
   on_attach = function(client, bufnr)
     if client.supports_method('textDocument/formatting') then
       vim.keymap.set('n', '<leader>f', function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        vim.cmd.Prettier()
       end, { buffer = bufnr, desc = '[lsp] format' })
 
       -- format on save
@@ -17,7 +16,7 @@ null_ls.setup({
         buffer = bufnr,
         group = group,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+          vim.cmd.Prettier()
         end,
         desc = '[lsp] format on save',
       })
@@ -25,7 +24,7 @@ null_ls.setup({
 
     if client.supports_method('textDocument/rangeFormatting') then
       vim.keymap.set('x', '<leader>f', function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        vim.cmd.Prettier()
       end, { buffer = bufnr, desc = '[lsp] format' })
     end
   end,
