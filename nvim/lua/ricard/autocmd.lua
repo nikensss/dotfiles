d500135810fbbebcbf95fe962acfb9100c3303be
Ricard Mallafre <rikitzzz@gmail.com>
2023-03-16 21:44:09 +0100

    feat: set zsh and sh files to bash

--- nvim/lua/ricard/autocmd.lua
+++ nvim/lua/ricard/autocmd.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'gitcommit', 'markdown', 'txt' },
+  group = vim.api.nvim_create_augroup('prose_writting', { clear = true }),
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
@@ -14,5 +15,13 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     if vim.bo[opts.buf].filetype == 'markdown' then
       vim.cmd.Prettier()
     end
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'zsh', 'sh' },
+  group = vim.api.nvim_create_augroup('set_ft_to_bash', { clear = true }),
+  callback = function()
+    vim.opt_local.ft = 'bash'
   end,
 })
