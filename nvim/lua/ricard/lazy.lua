3e345649ce0b531d0f74c9b7005ed8f2ed1d2c02
Ricard Mallafre <rikitzzz@gmail.com>
2023-03-27 16:40:26 +0200

    feat: rainbow csv plugin

--- nvim/lua/ricard/lazy.lua
+++ nvim/lua/ricard/lazy.lua
@@ -140,4 +140,23 @@ require('lazy').setup({
   { 'catppuccin/nvim', name = 'catppuccin' },
   'github/copilot.vim',
   'shime/vim-livedown',
+  {
+    'cameron-wags/rainbow_csv.nvim',
+    config = true,
+    ft = {
+      'csv',
+      'tsv',
+      'csv_semicolon',
+      'csv_whitespace',
+      'csv_pipe',
+      'rfc_csv',
+      'rfc_semicolon'
+    },
+    cmd = {
+      'RainbowDelim',
+      'RainbowDelimSimple',
+      'RainbowDelimQuoted',
+      'RainbowMultiDelim'
+    }
+  }
 })
