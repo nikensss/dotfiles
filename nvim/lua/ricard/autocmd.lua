1665d4f94493d166f5872a34116741f1ea57466b
Ricard Mallafre <rikitzzz@gmail.com>
2023-04-06 10:01:52 +0200

    fix: remove md autocmd to format
    To format md files on save there must be a prettier config file.

--- nvim/lua/ricard/autocmd.lua
+++ nvim/lua/ricard/autocmd.lua
@@ -8,16 +8,6 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
-vim.api.nvim_create_autocmd('BufWritePre', {
-  desc = 'format markdown on write using prettier',
-  group = vim.api.nvim_create_augroup('md_prettier_on_save', { clear = true }),
-  callback = function(opts)
-    if vim.bo[opts.buf].filetype == 'markdown' then
-      vim.cmd.Prettier()
-    end
-  end,
-})
-
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'zsh', 'sh' },
   group = vim.api.nvim_create_augroup('set_ft_to_bash', { clear = true }),
