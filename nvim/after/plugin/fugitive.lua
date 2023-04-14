a2bf6aeb9e423b853946ca9af21ed9174b1a2a3a
Ricard Mallafre <rikitzzz@gmail.com>
2023-04-14 11:50:17 +0200

    feat: git remaps
    Remaps to browse file history and to select versions in merge conflicts.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,3 +1,9 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 vim.keymap.set('n', '<leader>gb', function() vim.cmd.Git('blame') end)
 vim.keymap.set('n', '<leader>gp', function() vim.cmd.Git('push') end)
+
+vim.keymap.set('n', '<leader><leader>gl', [[ :diffget //3<CR> ]])
+vim.keymap.set('n', '<leader><leader>gh', [[ :diffget //2<CR> ]])
+
+vim.keymap.set('n', '<leader>gl', [[ :0Gclog<CR> ]])
+vim.keymap.set('v', '<leader>gl', [[ :Gclog<CR> ]])
