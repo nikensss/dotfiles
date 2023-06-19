e0e30e9bc7a2c594d27c6f044d4e1cfe46ab73d3
Ricard Mallafre <rikitzzz@gmail.com>
2023-06-19 12:07:38 +0200

    feat: script to control releases

--- nvim/lua/ricard/remap.lua
+++ nvim/lua/ricard/remap.lua
@@ -23,7 +23,7 @@ vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format)
 
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod u+x %<CR>', { silent = true })
 
 -- increment/decrement amount
 vim.keymap.set('n', '+', '<C-a>')
