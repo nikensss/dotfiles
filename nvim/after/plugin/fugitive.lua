49daa7cc399128669f1fa76da6ae3cc375f44dc8
Ricard Mallafre <rikitzzz@gmail.com>
2023-06-20 15:48:42 +0200

    feat: keymap to show vdiff with previous file version

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -7,3 +7,29 @@ vim.keymap.set('n', '<leader><leader>gh', [[ :diffget //2<CR> ]])
 
 vim.keymap.set('n', '<leader>gl', [[ :0Gclog<CR> ]])
 vim.keymap.set('v', '<leader>gl', [[ :Gclog<CR> ]])
+
+vim.keymap.set('n', '<leader><leader>gd', function()
+  local current_file = vim.fn.expand('%')
+  local commit_hash = current_file:match("/.git//([0-9a-fA-F]+)")
+
+  if not commit_hash then
+    return
+  end
+
+  local qf_list = vim.fn.getqflist()
+  for i, item in ipairs(qf_list) do
+    local qf_list_item_hash = item.module:match("([0-9a-fA-F]+)")
+
+    if commit_hash:find("^" .. qf_list_item_hash) then
+      local next_commit_hash = qf_list[i + 1] and qf_list[i + 1].module:match("([0-9a-fA-F]+)")
+      if next_commit_hash then
+        vim.cmd('Gvdiffsplit ' .. next_commit_hash)
+      else
+        print("no next commit in quickfix list")
+      end
+      return
+    end
+  end
+
+  print("current commit not found in quickfix list")
+end)
