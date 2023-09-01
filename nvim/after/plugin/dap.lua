c078c553d0c20289c2a3f273c6d58dbc72e3df89
Ricard Mallafre <rikitzzz@gmail.com>
2023-09-01 14:50:22 +0200

    chore: unicode characters for debugger in neovim

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -44,10 +44,10 @@ dap.configurations.typescript = {
 
 require('dap').set_log_level('INFO')
 dap.defaults.fallback.terminal_win_cmd = '20split new'
-vim.fn.sign_define('DapStopped', { text = '', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '⏵', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define('DapBreakpoint', { text = '⏹', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define('DapBreakpointCondition', { text = '⏸', texthl = '', linehl = '', numhl = '' })
-vim.fn.sign_define('DapBreakpointRejected', { text = '❌', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointRejected', { text = '×', texthl = '', linehl = '', numhl = '' })
 
 vim.keymap.set('n', '<leader>db', function()
   require('dap').toggle_breakpoint()
