71ca9351e6426f454327d9b66df7f0d5abb4d983
Ricard Mallafre <rikitzzz@gmail.com>
2023-10-18 15:30:27 +0200

    feat: use typescript-tools as tsserver

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,8 +1,8 @@
 -- import lspconfig plugin
-local lspconfig = require("lspconfig")
+local lspconfig = require('lspconfig')
 
 -- import cmp-nvim-lsp plugin
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
 local keymap = vim.keymap -- for conciseness
 
@@ -11,47 +11,47 @@ local on_attach = function(client, bufnr)
 	opts.buffer = bufnr
 
 	-- set keybinds
-	opts.desc = "Show LSP references"
-	keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+	opts.desc = 'Show LSP references'
+	keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts) -- show definition, references
 
-	opts.desc = "Go to declaration"
-	keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+	opts.desc = 'Go to declaration'
+	keymap.set('n', 'gD', vim.lsp.buf.declaration, opts) -- go to declaration
 
-	opts.desc = "Show LSP definitions"
-	keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>zz", opts) -- show lsp definitions
+	opts.desc = 'Show LSP definitions'
+	keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>zz', opts) -- show lsp definitions
 
-	opts.desc = "Show LSP implementations"
-	keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+	opts.desc = 'Show LSP implementations'
+	keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) -- show lsp implementations
 
-	opts.desc = "Show LSP type definitions"
-	keymap.set("n", "gy", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+	opts.desc = 'Show LSP type definitions'
+	keymap.set('n', 'gy', '<cmd>Telescope lsp_type_definitions<CR>', opts) -- show lsp type definitions
 
-	opts.desc = "See available code actions"
-	keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+	opts.desc = 'See available code actions'
+	keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-	opts.desc = "Smart rename"
-	keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+	opts.desc = 'Smart rename'
+	keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- smart rename
 
-	opts.desc = "Show buffer diagnostics"
-	keymap.set("n", "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+	opts.desc = 'Show buffer diagnostics'
+	keymap.set('n', '<leader>sd', '<cmd>Telescope diagnostics bufnr=0<CR>', opts) -- show  diagnostics for file
 
-	opts.desc = "Show line diagnostics"
-	keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts) -- show diagnostics for line
+	opts.desc = 'Show line diagnostics'
+	keymap.set('n', '<leader>ld', vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-	opts.desc = "Go to previous diagnostic"
-	keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+	opts.desc = 'Go to previous diagnostic'
+	keymap.set('n', '[d', vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-	opts.desc = "Go to next diagnostic"
-	keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+	opts.desc = 'Go to next diagnostic'
+	keymap.set('n', ']d', vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-	opts.desc = "Show documentation for what is under cursor"
-	keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+	opts.desc = 'Show documentation for what is under cursor'
+	keymap.set('n', 'K', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-	opts.desc = "Show signature help for what is under cursor"
-	keymap.set("n", "<C-k>", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+	opts.desc = 'Show signature help for what is under cursor'
+	keymap.set('n', '<C-k>', vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-	opts.desc = "Restart LSP"
-	keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+	opts.desc = 'Restart LSP'
+	keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts) -- mapping to restart lsp if necessary
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
@@ -59,93 +59,93 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Change the Diagnostic symbols in the sign column (gutter)
 -- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = 'DiagnosticSign' .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 end
 
 -- configure html server
-lspconfig["html"].setup({
+lspconfig['html'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 })
 
 -- configure typescript server with plugin
-lspconfig["tsserver"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
+-- lspconfig["tsserver"].setup({
+-- 	capabilities = capabilities,
+-- 	on_attach = on_attach,
+-- })
 
 -- configure css server
-lspconfig["cssls"].setup({
+lspconfig['cssls'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 })
 
 -- configure tailwindcss server
-lspconfig["tailwindcss"].setup({
+lspconfig['tailwindcss'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 })
 
 -- configure prisma orm server
-lspconfig["prismals"].setup({
+lspconfig['prismals'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 })
 
 -- configure graphql language server
-lspconfig["graphql"].setup({
+lspconfig['graphql'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
-	filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+	filetypes = { 'graphql', 'gql', 'svelte', 'typescriptreact', 'javascriptreact' },
 })
 
 -- configure sql language server
-lspconfig["sqlls"].setup({
+lspconfig['sqlls'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
-	filetypes = { "sql" },
+	filetypes = { 'sql' },
 })
 
 -- configure python server
-lspconfig["pyright"].setup({
+lspconfig['pyright'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 })
 
 -- configure lua server (with special settings)
-lspconfig["lua_ls"].setup({
+lspconfig['lua_ls'].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = { -- custom settings for lua
 		Lua = {
 			-- make the language server recognize "vim" global
 			diagnostics = {
-				globals = { "vim" },
+				globals = { 'vim' },
 			},
 			workspace = {
 				-- make language server aware of runtime files
 				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
+					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+					[vim.fn.stdpath('config') .. '/lua'] = true,
 				},
 			},
 		},
 	},
 })
 
-local rt = require("rust-tools")
+local rt = require('rust-tools')
 
 rt.setup({
 	server = {
 		on_attach = function(_, bufnr)
 			on_attach(_, bufnr)
 			-- Hover actions
-			keymap.set("n", "<leader>ha", rt.hover_actions.hover_actions, { buffer = bufnr })
+			keymap.set('n', '<leader>ha', rt.hover_actions.hover_actions, { buffer = bufnr })
 			-- Code action groups
-			keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+			keymap.set('n', '<leader>ca', rt.code_action_group.code_action_group, { buffer = bufnr })
 		end,
 	},
 })
--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,17 +1,17 @@
-local mason = require("mason")
+local mason = require('mason')
 
 -- import mason-lspconfig
-local mason_lspconfig = require("mason-lspconfig")
+local mason_lspconfig = require('mason-lspconfig')
 
-local mason_tool_installer = require("mason-tool-installer")
+local mason_tool_installer = require('mason-tool-installer')
 
 -- enable mason and configure icons
 mason.setup({
 	ui = {
 		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
+			package_installed = '✓',
+			package_pending = '➜',
+			package_uninstalled = '✗',
 		},
 	},
 })
@@ -19,17 +19,17 @@ mason.setup({
 mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
-		"bashls",
-		"cssls",
-		"graphql",
-		"html",
-		"lua_ls",
-		"prismals",
-		"pyright",
-		"rust_analyzer",
-		"sqlls",
-		"tailwindcss",
-		"tsserver",
+		'bashls',
+		'cssls',
+		'graphql',
+		'html',
+		'lua_ls',
+		'prismals',
+		'pyright',
+		'rust_analyzer',
+		'sqlls',
+		'tailwindcss',
+		-- 'tsserver',
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
@@ -37,12 +37,12 @@ mason_lspconfig.setup({
 
 mason_tool_installer.setup({
 	ensure_installed = {
-		"black", -- python formatter
-		"eslint_d", -- js linter
-		"isort", -- python formatter
-		"prettier", -- prettier formatter
-		"pylint", -- python linter
-		"stylua", -- lua formatter
-		"sql-formatter", -- sql formatter
+		'black', -- python formatter
+		'eslint_d', -- js linter
+		'isort', -- python formatter
+		'prettier', -- prettier formatter
+		'pylint', -- python linter
+		'stylua', -- lua formatter
+		'sql-formatter', -- sql formatter
 	},
 })
