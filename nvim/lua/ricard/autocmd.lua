a643889956328f506584713ad295edca8eaf92fc
Ricard Mallafre <rikitzzz@gmail.com>
2023-10-26 15:04:07 +0200

    fix: remove vertical columns in dbout files

--- nvim/lua/ricard/autocmd.lua
+++ nvim/lua/ricard/autocmd.lua
@@ -1,17 +1,25 @@
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'gitcommit', 'markdown', 'txt' },
-  group = vim.api.nvim_create_augroup('prose_writting', { clear = true }),
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-    vim.opt_local.textwidth = 79
-  end,
+	pattern = { 'gitcommit', 'markdown', 'txt' },
+	group = vim.api.nvim_create_augroup('prose_writting', { clear = true }),
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+		vim.opt_local.textwidth = 79
+	end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'zsh', 'sh' },
-  group = vim.api.nvim_create_augroup('set_ft_to_bash', { clear = true }),
-  callback = function()
-    vim.opt_local.ft = 'bash'
-  end,
+	pattern = { 'zsh', 'sh' },
+	group = vim.api.nvim_create_augroup('set_ft_to_bash', { clear = true }),
+	callback = function()
+		vim.opt_local.ft = 'bash'
+	end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'dbout' },
+	group = vim.api.nvim_create_augroup('dbout_filetype_settings', { clear = true }),
+	callback = function()
+		vim.opt.colorcolumn = '0'
+	end,
 })
