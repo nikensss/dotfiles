cabfb6962b145aca471c548a4ea860aa9ce2ba22
Ricard Mallafre <rikitzzz@gmail.com>
2023-11-02 09:43:30 +0100

    fix: trying to get snippets to work

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,20 +1,12 @@
-local cmp = require('cmp')
-
-local luasnip = require('luasnip')
-
-local lspkind = require('lspkind')
-
--- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 require('luasnip.loaders.from_vscode').lazy_load()
 
+local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
 cmp.setup({
-	completion = {
-		completeopt = 'menu,menuone,preview,noselect',
-	},
 	snippet = { -- configure how nvim-cmp interacts with snippet engine
 		expand = function(args)
-			luasnip.lsp_expand(args.body)
+			require('luasnip').lsp_expand(args.body)
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
@@ -30,16 +22,15 @@ cmp.setup({
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' }, -- snippets
+	}, {
 		{ name = 'buffer' }, -- text within current buffer
 		{ name = 'path' }, -- file system paths
 	}),
 	-- configure lspkind for vs-code like pictograms in completion menu
 	formatting = {
-		format = lspkind.cmp_format({
+		format = require('lspkind').cmp_format({
 			maxwidth = 50,
 			ellipsis_char = '...',
 		}),
 	},
 })
-
-require('luasnip.loaders.from_vscode').lazy_load()
