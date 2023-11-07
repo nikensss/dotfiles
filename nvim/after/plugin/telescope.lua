05d1ca6b44daf2aa47264e59400c5112060ef7d2
Ricard Mallafre <rikitzzz@gmail.com>
2023-11-07 10:11:24 +0100

    fix: explain keymaps for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,20 +2,45 @@ local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 local options = { noremap = true, silent = true }
 
+options.desc = '[s]earch with [g]rep'
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, options)
+
+options.desc = '[f]ind [c]urrent word'
 vim.keymap.set('n', '<leader>fc', builtin.grep_string, options)
+
+options.desc = '[s]earch [f]iles'
 vim.keymap.set('n', '<leader>sf', function()
 	builtin.find_files({ hidden = true })
 end, options)
+
+options.desc = '[s]earch in [p]roject'
 vim.keymap.set('n', '<leader>sp', builtin.git_files, options)
+
+options.desc = '[s]how [b]uffers'
 vim.keymap.set('n', '<leader>sb', builtin.buffers, options)
+
+options.desc = '[s]earch [h]elp'
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, options)
+
+options.desc = '[s]how [d]iagnostics'
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, options)
+
+options.desc = '[s]earch in [c]urrent buffer (fuzzy find)'
 vim.keymap.set('n', '<leader>sc', builtin.current_buffer_fuzzy_find, options)
+
+options.desc = '[s]how [k]eymaps'
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, options)
+
+options.desc = '[c]ommand [h]istory'
 vim.keymap.set('n', '<leader>ch', builtin.command_history, options)
+
+options.desc = '[c]urrent buffer [s]ymbols'
 vim.keymap.set('n', '<leader>cs', builtin.lsp_document_symbols, options)
+
+options.desc = '[s]ymbols in [w]orkspace'
 vim.keymap.set('n', '<leader>sw', builtin.lsp_workspace_symbols, options)
+
+options.desc = 'dynamic workspace [[s]]ymbols'
 vim.keymap.set('n', '<leader>ss', builtin.lsp_dynamic_workspace_symbols, options)
 
 require('telescope').setup({
