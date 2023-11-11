f414a7595bf3cf78017846f97e78b4067721f94d
Ricard Mallafre <rikitzzz@gmail.com>
2023-11-11 23:25:19 +0100

    chore: formatting

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,6 +1,10 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-vim.keymap.set('n', '<leader>gb', function() vim.cmd.Git('blame') end)
-vim.keymap.set('n', '<leader>gp', function() vim.cmd.Git('push') end)
+vim.keymap.set('n', '<leader>gb', function()
+	vim.cmd.Git('blame')
+end)
+vim.keymap.set('n', '<leader>gp', function()
+	vim.cmd.Git('push')
+end)
 
 vim.keymap.set('n', '<leader><leader>gl', [[ :diffget //3<CR> ]])
 vim.keymap.set('n', '<leader><leader>gh', [[ :diffget //2<CR> ]])
@@ -9,27 +13,30 @@ vim.keymap.set('n', '<leader>gl', [[ :0Gclog<CR> ]])
 vim.keymap.set('v', '<leader>gl', [[ :Gclog<CR> ]])
 
 vim.keymap.set('n', '<leader><leader>gd', function()
-  local current_file = vim.fn.expand('%')
-  local commit_hash = current_file:match("/.git//([0-9a-fA-F]+)")
+	local current_file = vim.fn.expand('%')
+	local commit_hash = current_file:match('/.git//([0-9a-fA-F]+)')
 
-  if not commit_hash then
-    return
-  end
+	if not commit_hash then
+		return
+	end
 
-  local qf_list = vim.fn.getqflist()
-  for i, item in ipairs(qf_list) do
-    local qf_list_item_hash = item.module:match("([0-9a-fA-F]+)")
+	local qf_list = vim.fn.getqflist()
+	for i, item in ipairs(qf_list) do
+		local qf_list_item_hash = item.module:match('([0-9a-fA-F]+)')
 
-    if commit_hash:find("^" .. qf_list_item_hash) then
-      local next_commit_hash = qf_list[i + 1] and qf_list[i + 1].module:match("([0-9a-fA-F]+)")
-      if next_commit_hash then
-        vim.cmd('Gvdiffsplit ' .. next_commit_hash)
-      else
-        print("no next commit in quickfix list")
-      end
-      return
-    end
-  end
+		if commit_hash:find('^' .. qf_list_item_hash) then
+			local next_commit_hash = qf_list[i + 1] and qf_list[i + 1].module:match('([0-9a-fA-F]+)')
+			if next_commit_hash then
+				vim.o.splitright = false
+				vim.cmd('Gvdiffsplit ' .. next_commit_hash)
+				vim.o.splitright = true
+			else
+				print('no next commit in quickfix list')
+			end
 
-  print("current commit not found in quickfix list")
+			return
+		end
+	end
+
+	print('current commit not found in quickfix list')
 end)
