07d6e531eff98383d7fbcf515242d028e64d3abd
Ricard Mallafre <rikitzzz@gmail.com>
2023-11-23 15:00:56 +0100

    feat: save sessions per project and branch

--- nvim/lua/ricard/functions.lua
+++ nvim/lua/ricard/functions.lua
@@ -9,4 +9,13 @@ function M.get_current_folder_name()
 	return parts[#parts]
 end
 
+function M.get_branch_name()
+	local branch = vim.fn.system('git branch --show-current 2> /dev/null | tr -d \'\n\'')
+	if branch ~= '' then
+		return branch
+	else
+		return ''
+	end
+end
+
 return M
--- nvim/lua/ricard/remap.lua
+++ nvim/lua/ricard/remap.lua
@@ -1,4 +1,9 @@
+local telescope = require('telescope.builtin')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
 local get_current_folder_name = require('ricard.functions').get_current_folder_name
+local get_branch_name = require('ricard.functions').get_branch_name
 
 vim.g.mapleader = ' '
 
@@ -78,16 +83,51 @@ vim.keymap.set('n', '<leader><leader>td', function()
 	vim.cmd.colorscheme('tokyonight-day')
 end)
 
+local function show_available_sessions(on_selection)
+	telescope.find_files({
+		prompt_title = 'Available sessions',
+		cwd = '~/.config/nvim/',
+		hidden = true,
+		no_ignore = true,
+		find_command = { 'find', '.', '-type', 'f', '-name', '*.session' },
+		layout_strategy = 'vertical',
+		layout_config = {
+			mirror = true,
+			width = 0.5,
+			prompt_position = 'top',
+		},
+		attach_mappings = function(prompt_bufnr, map)
+			on_selection(prompt_bufnr, map)
+			return true
+		end,
+	})
+end
+
+local function pick_session()
+	show_available_sessions(function(prompt_bufnr, map)
+		map({ 'i', 'n' }, '<CR>', function()
+			local selection = action_state.get_selected_entry()
+			actions.close(prompt_bufnr)
+			vim.cmd('source ~/.config/nvim/' .. string.sub(selection.value, 3))
+		end)
+	end)
+end
+
 vim.keymap.set('n', '<leader>ls', function()
-	local session = '~/.config/nvim/' .. get_current_folder_name() .. '.session'
+	local session = '~/.config/nvim/' .. get_current_folder_name() .. '__' .. get_branch_name() .. '.session'
 	if vim.fn.filereadable(vim.fn.expand(session)) == 1 then
 		vim.cmd('source ' .. session)
 		print('loaded session from ' .. session)
+	else
+		pick_session()
 	end
 end, { silent = true, desc = '[l]oad [s]ession' })
 
+vim.keymap.set('n', '<leader>ps', pick_session, { silent = true, desc = '[p]ick [s]ession' })
+
 vim.keymap.set('n', '<leader>ms', function()
-	local target = vim.fn.expand('~/.config/nvim/' .. get_current_folder_name() .. '.session')
+	local target =
+		vim.fn.expand('~/.config/nvim/' .. get_current_folder_name() .. '__' .. get_branch_name() .. '.session')
 	vim.cmd('mksession! ' .. target)
 	print('session saved to ' .. target)
 end, { silent = true, desc = '[m]ake [s]ession' })
