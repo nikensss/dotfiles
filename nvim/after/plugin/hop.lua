dc5cfef8398da19bb084aff304040dfc1e21d594
Ricard Mallafre <rikitzzz@gmail.com>
2023-11-29 13:11:28 +0100

    feat: lazy load rest-nvim and new hop keymaps

--- nvim/after/plugin/hop.lua
+++ nvim/after/plugin/hop.lua
@@ -1,6 +1,14 @@
 require('hop').setup()
 
-vim.keymap.set('n', 's', vim.cmd.HopWord)
-vim.keymap.set('n', 'S', vim.cmd.HopWordMW)
-vim.keymap.set('n', '<leader>s', vim.cmd.HopChar1)
-vim.keymap.set('n', '<leader>S', vim.cmd.HopChar1MW)
+vim.keymap.set('n', '<TAB>', vim.cmd.HopWord)
+vim.keymap.set('n', '<leader>j', vim.cmd.HopWord)
+
+vim.keymap.set('n', '<S-TAB>', vim.cmd.HopWordMW)
+vim.keymap.set('n', '<leader>k', vim.cmd.HopWordMW)
+
+vim.keymap.set('n', '<leader>hc', vim.cmd.HopChar1)
+vim.keymap.set('n', '<M-j>', vim.cmd.HopChar1)
+
+-- vim.keymap.set('n', '<leader>S', vim.cmd.HopChar1MW)
+vim.keymap.set('n', '<leader>hx', vim.cmd.HopChar1MW)
+vim.keymap.set('n', '<M-k>', vim.cmd.HopChar1MW)
