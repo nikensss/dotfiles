4f32819ef257d4289635c845829247f9d0dcb345
Ricard Mallafre <rikitzzz@gmail.com>
2023-12-05 16:16:48 +0100

    fix: tweaked hop keymaps

--- nvim/after/plugin/hop.lua
+++ nvim/after/plugin/hop.lua
@@ -1,15 +1,12 @@
 require('hop').setup()
 
 vim.keymap.set('n', 's', vim.cmd.HopWord, { desc = '[hop] hop word' })
-vim.keymap.set('n', '<leader>j', vim.cmd.HopWord, { desc = '[hop] hop word' })
-
 vim.keymap.set('n', 'S', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
-vim.keymap.set('n', '<leader>k', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
 
-vim.keymap.set('n', '<leader>hc', vim.cmd.HopChar1, { desc = '[hop] hop char' })
-vim.keymap.set('n', '<M-j>', vim.cmd.HopChar1, { desc = '[hop] hop char' })
+vim.keymap.set('n', '<leader>j', vim.cmd.HopWord, { desc = '[hop] hop word' })
+vim.keymap.set('n', '<M-j>', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
 
-vim.keymap.set('n', '<leader>hx', vim.cmd.HopChar1MW, { desc = '[hop] hop char multi window' })
+vim.keymap.set('n', '<leader>k', vim.cmd.HopChar1, { desc = '[hop] hop char' })
 vim.keymap.set('n', '<M-k>', vim.cmd.HopChar1MW, { desc = '[hop] hop char multi window' })
 
 vim.keymap.set('n', '<M-l>', vim.cmd.HopLine, { desc = '[hop] hop line' })
