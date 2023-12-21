ee24dbb8e58b1132b52cdc2434de7d0007417362
Ricard Mallafre <rikitzzz@gmail.com>
2023-12-21 17:25:23 +0100

    feat: open dadbod in a different tab

--- nvim/after/plugin/dadbod-ui.lua
+++ nvim/after/plugin/dadbod-ui.lua
@@ -1,1 +1,10 @@
-vim.keymap.set('n', '<leader>pg', vim.cmd.DBUIToggle, { desc = 'DBUI' })
+vim.keymap.set('n', '<leader>pg', function()
+	local tab_index = require('ricard.functions').find_tab_with_buffer('dbui')
+
+	if tab_index == nil then
+		vim.cmd.tabe()
+		vim.cmd.DBUIToggle()
+	else
+		vim.api.nvim_set_current_tabpage(tab_index)
+	end
+end, { desc = 'DBUI' })
--- nvim/lua/ricard/functions.lua
+++ nvim/lua/ricard/functions.lua
@@ -18,4 +18,38 @@ function M.get_branch_name()
 	end
 end
 
+function M.get_buffers_in_tabs()
+	local tabs_buffers = {}
+	local tabs = vim.api.nvim_list_tabpages()
+
+	for tab_index, tab in ipairs(tabs) do
+		local windows = vim.api.nvim_tabpage_list_wins(tab)
+		local buffer_names = {}
+
+		for _, win in ipairs(windows) do
+			local buf = vim.api.nvim_win_get_buf(win)
+			local name = vim.api.nvim_buf_get_name(buf)
+			table.insert(buffer_names, name)
+		end
+
+		tabs_buffers[tab_index] = buffer_names
+	end
+
+	return tabs_buffers
+end
+
+function M.find_tab_with_buffer(buffer_name)
+	local buffers_in_tabs = M.get_buffers_in_tabs()
+
+	for tab_index, buffers in pairs(buffers_in_tabs) do
+		for _, name in ipairs(buffers) do
+			if name:find(buffer_name) then -- Check if the buffer name contains the specified name
+				return tab_index -- Return the tab index
+			end
+		end
+	end
+
+	return nil
+end
+
 return M
