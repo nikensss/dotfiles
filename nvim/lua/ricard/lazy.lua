7dcbd8675d54f5dc15fc614112699ebac1ddcc43
Ricard Mallafre <rikitzzz@gmail.com>
2024-01-17 10:57:45 +0100

    feat: added undotree

--- nvim/lua/ricard/lazy.lua
+++ nvim/lua/ricard/lazy.lua
@@ -323,4 +323,13 @@ require('lazy').setup({
 			)
 		end,
 	},
+	{
+		'mbbill/undotree',
+		keys = { '<leader>ut' },
+		config = function()
+			local options = { silent = true, noremap = true, desc = '[undotree] toggle undotree' }
+			vim.keymap.set('n', '<leader>ut', '<cmd>UndotreeToggle<cr>', options)
+			vim.g.undotree_SetFocusWhenToggle = 1
+		end,
+	},
 })
