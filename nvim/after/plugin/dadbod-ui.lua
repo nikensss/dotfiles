256c4f31347bd6076d117ac5d7d3a5e84646cc8a
Ricard Mallafre <rikitzzz@gmail.com>
2024-01-18 12:19:49 +0100

    feat: swap between dbui and previous tab

--- nvim/after/plugin/dadbod-ui.lua
+++ nvim/after/plugin/dadbod-ui.lua
@@ -1,10 +1,19 @@
-vim.keymap.set('n', '<leader>pg', function()
-	local tab_index = require('ricard.functions').find_tab_with_buffer('dbui')
+vim.g.dbui_previous_tab = nil
 
-	if tab_index == nil then
+vim.keymap.set('n', '<leader>pg', function()
+	local db_ui_tab_index = require('ricard.functions').find_tab_with_buffer('dbui')
+
+	if db_ui_tab_index == nil then
+		vim.g.dbui_previous_tab = vim.api.nvim_get_current_tabpage()
 		vim.cmd.tabe()
 		vim.cmd.DBUIToggle()
+	elseif db_ui_tab_index == vim.api.nvim_get_current_tabpage() then
+		if vim.g.dbui_previous_tab == nil then
+			return
+		end
+		vim.api.nvim_set_current_tabpage(vim.g.dbui_previous_tab)
 	else
-		vim.api.nvim_set_current_tabpage(tab_index)
+		vim.g.dbui_previous_tab = vim.api.nvim_get_current_tabpage()
+		vim.api.nvim_set_current_tabpage(db_ui_tab_index)
 	end
 end, { desc = 'DBUI' })
