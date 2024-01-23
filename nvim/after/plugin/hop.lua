9eb4c24a430f3268fb1809d8d9dafd27355b52e1
Ricard Mallafre <rikitzzz@gmail.com>
2024-01-23 10:32:25 +0100

    fix: copilot suggestions with a different mapping

--- nvim/after/plugin/hop.lua
+++ nvim/after/plugin/hop.lua
@@ -2,6 +2,7 @@ require('hop').setup()
 
 vim.keymap.set('n', 's', vim.cmd.HopWord, { desc = '[hop] hop word' })
 vim.keymap.set('n', 'S', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
+vim.keymap.set('n', '<C-s>', vim.cmd.HopChar1, { desc = '[hop] hop char' })
 
 vim.keymap.set('n', '<leader>j', vim.cmd.HopWord, { desc = '[hop] hop word' })
 vim.keymap.set('n', '<M-j>', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
