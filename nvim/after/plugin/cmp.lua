ffc632bc942e99dad0ec8a1259d9a8f25bb60c14
Ricard Mallafre <rikitzzz@gmail.com>
2024-02-02 13:30:15 +0100

    fix: borders for cmp windows

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -9,6 +9,10 @@ cmp.setup({
 			require('luasnip').lsp_expand(args.body)
 		end,
 	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-e>'] = cmp.mapping.close(),
