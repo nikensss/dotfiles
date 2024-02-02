947dca4dad9d43bbbf359618acacc3c0bfdf3886
Ricard Mallafre <rikitzzz@gmail.com>
2024-02-02 14:10:52 +0100

    fix: show in lualine what I can't get noice to show properly

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -10,7 +10,16 @@ require('lualine').setup({
 		lualine_a = { 'mode' },
 		lualine_b = { 'branch', 'diff', 'diagnostics' },
 		lualine_c = { { 'filename', file_status = true, path = 1 } },
-		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_x = {
+			{
+				require('noice').api.statusline.mode.get,
+				cond = require('noice').api.statusline.mode.has,
+				color = { fg = '#ff9e64' },
+			},
+			'encoding',
+			'fileformat',
+			'filetype',
+		},
 		lualine_y = { 'progress' },
 		lualine_z = { 'location' },
 	},
