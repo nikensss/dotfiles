63f2891dabc23aa69761c768428e6205295e8585
Ricard Mallafre <rikitzzz@gmail.com>
2024-02-29 16:10:39 +0100

    fix: remaps for window navigation

--- nvim/lua/ricard/remap.lua
+++ nvim/lua/ricard/remap.lua
@@ -35,6 +35,13 @@ vim.keymap.set('n', '<leader>x', '<cmd>!chmod u+x %<CR>', { silent = true })
 vim.keymap.set('n', '+', '<C-a>')
 vim.keymap.set('n', '-', '<C-x>')
 
+-- window navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+vim.keymap.set('n', '<C-v>', '<C-w><C-v>')
+
 -- resize windows
 vim.keymap.set('n', '<leader>+', '5<C-w>>')
 vim.keymap.set('n', '<leader>-', '5<C-w><')
