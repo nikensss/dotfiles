d7f89d7cb723932ed73bf062ecfaf04489aa360b
Ricard Mallafre <rikitzzz@gmail.com>
2024-05-25 13:30:32 +0200

    feat: add deno support
    This reverts commit c7b347a995f3ab0b0cbf057d41d105cb7c9f303c.

--- nvim/after/plugin/nvim-lint.lua
+++ nvim/after/plugin/nvim-lint.lua
@@ -1,3 +1,5 @@
+local is_deno_project = require('ricard.functions').is_deno_project
+
 local lint = require('lint')
 
 lint.linters_by_ft = {
@@ -15,6 +17,29 @@ local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
 	group = lint_augroup,
 	callback = function()
+		if is_deno_project() then
+			lint.linters_by_ft = {
+				javascript = { 'deno' },
+				typescript = { 'deno' },
+				javascriptreact = { 'deno' },
+				typescriptreact = { 'deno' },
+				svelte = { 'deno' },
+				python = { 'pylint' },
+			}
+			lint.try_lint()
+			return
+		else
+			lint.linters_by_ft = {
+				css = { 'css-lsp' },
+				javascript = { 'eslint' },
+				typescript = { 'eslint' },
+				javascriptreact = { 'eslint' },
+				typescriptreact = { 'eslint' },
+				svelte = { 'eslint' },
+				python = { 'pylint' },
+			}
+		end
+
 		lint.try_lint()
 	end,
 })
