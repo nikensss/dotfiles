051f31fc77c646fe490056193e4cbb3b1d0271d2
Ricard Mallafre <rikitzzz@gmail.com>
2024-09-11 14:08:30 +0200

    fix: <c-n> and <c-p> to navigate files in diffview

--- nvim/lua/ricard/lazy.lua
+++ nvim/lua/ricard/lazy.lua
@@ -437,7 +437,33 @@ require('lazy').setup({
 		end,
 	},
 	'segeljakt/vim-silicon',
-	'sindrets/diffview.nvim',
+	{
+		'sindrets/diffview.nvim',
+		config = function()
+			local actions = require('diffview.actions')
+
+			require('diffview').setup({
+				keymaps = {
+					file_panel = {
+						['<tab>'] = false,
+						['<s-tab>'] = false,
+						{
+							'n',
+							'<c-n>',
+							actions.select_next_entry,
+							{ desc = 'Open the diff for the next file' },
+						},
+						{
+							'n',
+							'<c-p>',
+							actions.select_prev_entry,
+							{ desc = 'Open the diff for the previous file' },
+						},
+					},
+				},
+			})
+		end,
+	},
 	'ziglang/zig.vim',
 	{
 		'pwntester/octo.nvim',
