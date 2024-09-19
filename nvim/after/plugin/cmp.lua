e5e61aaf5d566fece78a66750a4641737960e91b
Ricard Mallafre <rikitzzz@gmail.com>
2024-09-19 11:11:34 +0200

    fix: scroll docs properly

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -57,3 +57,15 @@ cmp.setup({
 		}),
 	},
 })
+
+vim.keymap.set({ 'n', 'i', 's' }, '<c-f>', function()
+	if not require('noice.lsp').scroll(4) then
+		return '<c-f>'
+	end
+end, { silent = true, expr = true })
+
+vim.keymap.set({ 'n', 'i', 's' }, '<c-b>', function()
+	if not require('noice.lsp').scroll(-4) then
+		return '<c-b>'
+	end
+end, { silent = true, expr = true })
