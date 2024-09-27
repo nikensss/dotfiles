8fa1fcc3cff1ebaa02c844345194ffbd612f91bd
Ricard Mallafre <rikitzzz@gmail.com>
2024-09-27 10:35:42 +0200

    feat: keymaps for copilot

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,5 +1,10 @@
-vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
-	expr = true,
-	replace_keycodes = false,
-})
+vim.keymap.set(
+	'i',
+	'<C-j>',
+	'copilot#Accept("\\<CR>")',
+	{ expr = true, replace_keycodes = false, desc = '[copilot] accept completion' }
+)
+vim.keymap.set('i', '<M-w>', '<Plug>(copilot-accept-word)', { noremap = false, desc = '[copilot] accept word' })
+vim.keymap.set('n', '<leader>cp', '<cmd>Copilot panel<CR>', { noremap = true, desc = '[copilot] open panel' })
+
 vim.g.copilot_no_tab_map = true
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,9 @@
 return {
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdateSync',
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter-context',
-			'nvim-treesitter/nvim-treesitter-refactor',
-			'nvim-treesitter/nvim-treesitter-textobjects',
-		},
-		config = function()
-			vim.keymap.set('n', '<leader>fq', '<cmd>NvimTreeClose<cr>', { desc = '[nvim-tree] close', silent = true })
-		end,
-	}
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdateSync',
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter-context',
+		'nvim-treesitter/nvim-treesitter-refactor',
+		'nvim-treesitter/nvim-treesitter-textobjects',
+	},
+}
