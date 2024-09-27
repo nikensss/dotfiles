e664247f83d5cb568d55f818d25a7a3ada1552bb
Ricard Mallafre <rikitzzz@gmail.com>
2024-09-27 11:44:59 +0200

    docs: added descriptions for some keymaps

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,13 +1,13 @@
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'git status' })
 vim.keymap.set('n', '<leader>gb', function()
 	vim.cmd.Git('blame')
-end)
+end, { desc = 'git blame' })
 vim.keymap.set('n', '<leader>gp', function()
 	vim.cmd.Git('push')
-end)
+end, { desc = 'git push' })
 
-vim.keymap.set('n', '<leader><leader>gl', [[ :diffget //3<CR> ]])
-vim.keymap.set('n', '<leader><leader>gh', [[ :diffget //2<CR> ]])
+vim.keymap.set('n', '<leader><leader>gl', [[ :diffget //3<CR> ]], { desc = '[git] keep theirs' })
+vim.keymap.set('n', '<leader><leader>gh', [[ :diffget //2<CR> ]], { desc = '[git] keep ours' })
 
 vim.keymap.set('n', '<leader>gl', [[ :0Gclog<CR> ]])
 vim.keymap.set('v', '<leader>gl', [[ :Gclog<CR> ]])
