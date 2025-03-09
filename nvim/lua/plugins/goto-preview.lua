3ff373c3e798a9811aaf774dbe4214063bcefacd
Ricard Mallafre <rikitzzz@gmail.com>
2025-03-09 13:01:31 +0100

    fix: added logger dependency for goto-preview

--- nvim/lua/plugins/goto-preview.lua
+++ nvim/lua/plugins/goto-preview.lua
@@ -1,13 +1,14 @@
 return {
-		'rmagatti/goto-preview',
-		config = function()
-			require('goto-preview').setup({
-				default_mappings = true,
-				height = 45,
-				post_open_hook = function()
-					local bufnr = vim.api.nvim_get_current_buf()
-					vim.keymap.set('n', 'q', '<C-w>q', { noremap = true, silent = true, buffer = bufnr })
-				end,
-			})
-		end,
-	}
+	'rmagatti/goto-preview',
+	dependencies = { 'rmagatti/logger.nvim' },
+	config = function()
+		require('goto-preview').setup({
+			default_mappings = true,
+			height = 45,
+			post_open_hook = function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				vim.keymap.set('n', 'q', '<C-w>q', { noremap = true, silent = true, buffer = bufnr })
+			end,
+		})
+	end,
+}
