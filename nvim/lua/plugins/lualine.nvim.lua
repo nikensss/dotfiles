fb9e92a4f3af7e1aea384faadc3b59d95b1fb6a9
Ricard Mallafre <rikitzzz@gmail.com>
2026-03-17 14:13:15 +0100

    fix: lualine colorscheme timing problem

--- nvim/lua/plugins/lualine.nvim.lua
+++ nvim/lua/plugins/lualine.nvim.lua
@@ -1,3 +1,81 @@
 return {
 		'nvim-lualine/lualine.nvim',
+		dependencies = { 'catppuccin/nvim', 'SmiteshP/nvim-navic' },
+		config = function()
+			local navic = require('nvim-navic')
+
+			require('lualine').setup({
+				options = {
+					icons_enabled = true,
+					theme = 'auto',
+					section_separators = { left = '', right = '' },
+					component_separators = { left = '', right = '' },
+					disabled_filetypes = {},
+				},
+				winbar = {
+					lualine_c = {
+						{
+							function()
+								if not navic.is_available() then
+									return ''
+								end
+
+								local loc = navic.get_location()
+								if loc == '' then
+									return ''
+								end
+								return loc
+							end,
+							cond = function()
+								return true
+							end,
+							draw_empty = true,
+						},
+					},
+				},
+				inactive_winbar = {
+					lualine_c = {
+						{
+							function()
+								if not navic.is_available() then
+									return ''
+								end
+
+								local loc = navic.get_location()
+								if loc == '' then
+									return ''
+								end
+								return loc
+							end,
+							cond = function()
+								return true
+							end,
+							draw_empty = true,
+						},
+					},
+				},
+				sections = {
+					lualine_a = { 'mode' },
+					lualine_b = { 'branch', 'diff', 'diagnostics' },
+					lualine_c = {
+						{ 'filename', file_status = true, path = 1 },
+					},
+					lualine_x = {
+						'encoding',
+						'fileformat',
+						'filetype',
+					},
+					lualine_y = { 'progress' },
+					lualine_z = { 'location' },
+				},
+				inactive_sections = {
+					lualine_a = { 'mode' },
+					lualine_b = { 'branch', 'diff', 'diagnostics' },
+					lualine_c = {
+						{ 'filename', file_status = true, path = 1 },
+					},
+				},
+				extensions = { 'fugitive' },
+			})
+		end,
 	}
