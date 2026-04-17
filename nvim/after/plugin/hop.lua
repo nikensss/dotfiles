095ebff374d1d84d2eaad4ccaa4eab63941f3606
Ricard Mallafre <rikitzzz@gmail.com>
2026-04-17 20:19:03 +0200

    fix(hop): new keymaps for hop

--- nvim/after/plugin/hop.lua
+++ nvim/after/plugin/hop.lua
@@ -2,12 +2,9 @@ require('hop').setup()
 
 vim.keymap.set('n', 's', vim.cmd.HopWord, { desc = '[hop] hop word' })
 vim.keymap.set('n', 'S', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
-vim.keymap.set('n', '<C-s>', vim.cmd.HopChar1, { desc = '[hop] hop char' })
 
 vim.keymap.set('n', '<leader>j', vim.cmd.HopWord, { desc = '[hop] hop word' })
-vim.keymap.set('n', '<M-j>', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
+vim.keymap.set('n', '<leader>J', vim.cmd.HopWordMW, { desc = '[hop] hop word multi window' })
 
-vim.keymap.set('n', '<leader>k', vim.cmd.HopChar1, { desc = '[hop] hop char' })
-vim.keymap.set('n', '<M-k>', vim.cmd.HopChar1MW, { desc = '[hop] hop char multi window' })
-
-vim.keymap.set('n', '<M-l>', vim.cmd.HopLine, { desc = '[hop] hop line' })
+vim.keymap.set('n', '<leader>k', vim.cmd.HopLine, { desc = '[hop] hop line' })
+vim.keymap.set('n', '<leader>K', vim.cmd.HopLineMW, { desc = '[hop] hop line multi window' })
