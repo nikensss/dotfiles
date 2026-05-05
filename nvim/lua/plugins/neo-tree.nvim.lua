86798221fa8fae986212f8c2077c7d6bcda8766d
Ricard Mallafre <rikitzzz@gmail.com>
2026-05-05 10:53:58 +0200

    fix: neo-tree keymaps

--- nvim/lua/plugins/neo-tree.nvim.lua
+++ nvim/lua/plugins/neo-tree.nvim.lua
@@ -1,21 +1,29 @@
 return {
-		'nvim-neo-tree/neo-tree.nvim',
-		branch = 'v3.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-			'MunifTanjim/nui.nvim',
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		},
-		opts = {
-			filesystem = {
-				follow_current_file = {
-					enabled = true,
-				},
-				hijack_netrw_behavior = 'disabled',
+	'nvim-neo-tree/neo-tree.nvim',
+	branch = 'v3.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+		'MunifTanjim/nui.nvim',
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	opts = {
+		filesystem = {
+			follow_current_file = {
+				enabled = true,
 			},
+			hijack_netrw_behavior = 'disabled',
+
 			window = {
-				position = 'float',
+				fuzzy_finder_mappings = {
+					['<esc>'] = 'close_keep_filter',
+					['<C-x>'] = 'close_clear_filter',
+				},
 			},
 		},
-	}
+
+		window = {
+			position = 'float',
+		},
+	},
+}
